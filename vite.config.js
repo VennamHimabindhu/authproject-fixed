11608970195ac5986f8c61d6142c0eca065430c4
vennam himabindu <vennamhimabindu27@gmail.com>
2025-06-06 09:01:33 +0530

    Fix build issues and successful local build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    preserveSymlinks: true,
-  },
-  optimizeDeps: {
-    include: ['react-router', 'react-router-dom'],
-  },
-})
+ resolve: {
+  alias: {},
+},
+
+});
